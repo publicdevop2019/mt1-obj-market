27365f0ada22d4fc236ec2bd118900b751d1c1c7
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-01-08 00:19:41 -0500

    update animation config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,48 +13,51 @@ import { OrderDetailComponent } from './pages/order-detail/order-detail.componen
 import { OrdersComponent } from './pages/orders/orders.component';
 import { PaymentsComponent } from './pages/payments/payments.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-
+/**
+ * @note remember to add animation in data {} to for new router mapping
+ */
 const routes: Routes = [
+
     {
         path: 'home',
         component: HomeComponent,
         data: {
             productListCategory: 'top',
             listView: 'simple',
-            animation: 'HomePage'
+            animation: 'home'
         }
     },
     {
         path: 'categories',
         component: CategoriesComponent,
-        data: { listView: 'full', animation: 'CategoriesPage' }
+        data: { listView: 'full', animation: 'categories' }
     },
     {
         path: 'categories/:category',
         component: ProductListComponent,
-        data: { productListCategory: 'byCategory' }
+        data: { productListCategory: 'byCategory', animation: 'category' }
     },
     {
         path: 'categories/:category/:productId',
         component: ProductDetailComponent,
-        data: { fab: 'goBack' }
+        data: { fab: 'goBack', animation: 'productDetail' }
     },
-    { path: 'cart', component: CartComponent, data: { animation: 'CartPage' } },
+    { path: 'cart', component: CartComponent, data: { animation: 'cart' } },
     {
         path: 'account',
         component: AccountComponent,
         data: { animation: 'AccountPage' }
     },
-    { path: 'order', component: OrderDetailComponent },
-    { path: 'orders', component: OrdersComponent },
-    { path: 'orders/:orderId', component: OrderDetailComponent },
-    { path: 'order-complete', component: OrderCompleteComponent },
-    { path: 'payment', component: FormPaymentComponent },
-    { path: 'payments', component: PaymentsComponent },
-    { path: 'payments/:paymentId', component: FormPaymentComponent },
-    { path: 'address', component: FormAddressComponent },
-    { path: 'addresses', component: AddressesComponent },
-    { path: 'addresses/:addressId', component: FormAddressComponent },
+    { path: 'order', component: OrderDetailComponent, data: { animation: 'order' } },
+    { path: 'orders', component: OrdersComponent, data: { animation: 'orders' } },
+    { path: 'orders/:orderId', component: OrderDetailComponent, data: { animation: 'orderDetail' } },
+    { path: 'order-complete', component: OrderCompleteComponent, data: { animation: 'orderComplete' } },
+    { path: 'payment', component: FormPaymentComponent, data: { animation: 'payment' } },
+    { path: 'payments', component: PaymentsComponent, data: { animation: 'payments' } },
+    { path: 'payments/:paymentId', component: FormPaymentComponent, data: { animation: 'paymentDetail' } },
+    { path: 'address', component: FormAddressComponent, data: { animation: 'address' } },
+    { path: 'addresses', component: AddressesComponent, data: { animation: 'addresses' } },
+    { path: 'addresses/:addressId', component: FormAddressComponent, data: { animation: 'addressDetail' } },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', component: HomeComponent }
 ];
@@ -63,4 +66,4 @@ const routes: Routes = [
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }

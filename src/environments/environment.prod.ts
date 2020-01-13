ebd8cef43fe99a9ebc8b6526d0d185fad840e7a4
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-01-12 22:17:14 -0500

    update env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   mode: 'online' as 'online' | 'offline',
-  getTokenUri: 'http://www.ngform.com:8111/oauth/token',
-  oauthRedirectUri: 'http://www.ngform.com/account',
   APP_ID: 'obj-market',
   APPP_SECRET_PUBLIC: '',
+  getTokenUri: 'http://www.ngform.com:8111/oauth/token',
+  oauthRedirectUri: 'http://www.ngform.com/account',
   authorzieUrl: 'https://www.ngform.com:8081',
   profileUrl:'http://www.ngform.com:8111',
   productUrl:'http://www.ngform.com:8111',

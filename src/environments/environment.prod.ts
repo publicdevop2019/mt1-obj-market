bdab6fa79cc974a02e244868541ce9e3154c3735
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-04-04 23:24:23 -0400

    update port number

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   APPP_SECRET_PUBLIC: '',
   getTokenUri: 'http://api.manytreetechnology.com:8111/auth-svc/oauth/token',
   oauthRedirectUri: 'http://shop.manytreetechnology.com:8111/account',
-  authorzieUrl: 'http://admin.manytreetechnology.com:8111:8111/authorize?response_type=code&',
+  authorzieUrl: 'http://api.manytreetechnology.com/authorize?response_type=code&',
   profileUrl:'http://api.manytreetechnology.com:8111/profile-svc',
   productUrl:'http://api.manytreetechnology.com:8111/product-svc',
   imageUrl:'http://api.manytreetechnology.com:8111/file-upload-svc/files',

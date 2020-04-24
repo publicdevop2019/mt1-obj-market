441bb361e85ce77552d585b1bd33a07ca40fec88
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-04-24 15:18:40 -0400

    add hello-world config

---
+++ src/environments/environment.hw.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  production: true,
+  mode: 'online' as 'online' | 'offline',
+  APP_ID: 'obj-market',
+  APPP_SECRET_PUBLIC: '',
+  authorzieUrl: 'http://localhost:4300/authorize?response_type=code&',
+  productUrl: 'http://localhost:8111/product-svc',
+  imageUrl: 'http://localhost:8111/file-upload-svc/files',
+  getTokenUri: 'http://localhost:8111/auth-svc/oauth/token',
+  oauthRedirectUri: 'http://localhost:4200/account',
+  profileUrl: 'http://localhost:8111/profile-svc',
+};
+

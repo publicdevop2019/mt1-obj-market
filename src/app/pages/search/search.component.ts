a856fe8cb38dd548bf2019f9e50818d3fa93c6fd
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-04-28 20:25:59 -0400

    expose query param to url

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { debounce, switchMap, filter, map, skip } from 'rxjs/operators';
 import { interval, Observable, Subscription } from 'rxjs';
@@ -6,6 +6,7 @@ import { HttpProxyService } from 'src/app/services/http-proxy.service';
 import { IProductSimple } from '../product-detail/product-detail.component';
 import { FormSearchComponent } from 'src/app/components/form-search/form-search.component';
 import { GhostService } from 'src/app/services/ghost.service';
+import { Router, ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -21,20 +22,39 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
   endOfPages = false;
   searchResults: IProductSimple[];
   ngAfterViewInit(): void {
+    // sub for search event
     this.sub0 = this.searchComponnent.searchForm.get('search').valueChanges
       .pipe(debounce(() => interval(1000)))
       .pipe(filter(el => this.invalidSearchParam(el)))
       .pipe(map(el => el.trim()))
       .pipe(switchMap(e => {
         this.searchKey = e;
+        this.pageNumber = 0;
+        this.endOfPages = false;
+        // update url search key
+        this.router.navigate(
+          [],
+          {
+            relativeTo: this.activeRoute,
+            queryParams: { key: this.searchKey },
+            queryParamsHandling: 'merge'
+          });
         return this._httpProxy.netImpl.searchProduct(e, this.pageNumber, this.pageSize)
       }))
       .subscribe(next => {
         this.searchResults = next;
       })
+    this.activeRoute.queryParamMap.subscribe(queryMaps => {
+      if (queryMaps.get('key')) {
+        const str = queryMaps.get('key')
+        this.searchKey = str;
+        this.searchComponnent.searchForm.get('search').setValue(this.searchKey);
+        this.changeRef.detectChanges();
+      }
+    });
   }
   @ViewChild('searchInput') searchComponnent: FormSearchComponent;
-  constructor(private _httpProxy: HttpProxyService, private ghostSvc: GhostService) {
+  constructor(private _httpProxy: HttpProxyService, private ghostSvc: GhostService, private router: Router, private activeRoute: ActivatedRoute, private changeRef: ChangeDetectorRef) {
   }
 
   ngOnDestroy(): void {
@@ -42,6 +62,7 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
     this.sub1.unsubscribe()
   }
   ngOnInit() {
+    // sub for infinite scroll
     this.sub1 = this.ghostSvc.productCardGhostObser
       .pipe(switchMap(() => {
         this.pageNumber++;

7c3aa0eee16dc90a756701b91f979ad47fd1a2ae
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-09-30 18:47:06 -0400

    update mock data

--- src/app/services/offline.interceptor.ts
+++ src/app/services/offline.interceptor.ts
@@ -11,6 +11,7 @@ import { mockProductSimple } from 'src/assets/mocks/mock-product-simples';
 import { mockCart } from 'src/assets/mocks/mock-cart';
 import { mockOrder } from 'src/assets/mocks/mock-order';
 import { mockFilter } from 'src/assets/mocks/mock-filter';
+import { mockAddress } from 'src/assets/mocks/mock-address';
 /**
  * use refresh token if call failed
  */
@@ -29,8 +30,8 @@ export class OfflineInterceptor implements HttpInterceptor {
                 }
                 if (req.url.includes('orders/')) {
                     let header = new HttpHeaders();
-                    let var0=header.set('Location', 'weixin?//wxpay/bizpayurl?appid=wx2421b1c4370ec43b&mch_id=10000100&nonce_str=aa43681f1b804d2eacfb2f9e0107aa59&product_id=836202480025600&time_stamp=1415949957&sign=512F68131DD251DA4A45DA79CC7EFE9D')
-                    return of(new HttpResponse({ status: 200, headers: var0 })).pipe(delay(this.DEFAULT_DELAY))
+                    let var0 = header.set('Location', 'weixin?//wxpay/bizpayurl?appid=wx2421b1c4370ec43b&mch_id=10000100&nonce_str=aa43681f1b804d2eacfb2f9e0107aa59&product_id=836202480025600&time_stamp=1415949957&sign=512F68131DD251DA4A45DA79CC7EFE9D')
+                    return of(new HttpResponse({ status: 200, headers: var0, body: { paymentStatus: true } })).pipe(delay(this.DEFAULT_DELAY))
                 }
                 return of(new HttpResponse({ status: 200 })).pipe(delay(this.DEFAULT_DELAY));
             }
@@ -53,21 +54,16 @@ export class OfflineInterceptor implements HttpInterceptor {
                 if (req.url.includes('catalogs')) {
                     return of(new HttpResponse({ status: 200, body: mockCatalog })).pipe(delay(this.DEFAULT_DELAY))
                 }
-                if (req.url.includes('addresses')) {
+                if (req.url.includes('addresses/user/')) {
+                    return of(new HttpResponse({ status: 200, body: mockAddress })).pipe(delay(this.DEFAULT_DELAY))
+                }
+                if (req.url.includes('addresses/user')) {
                     return of(new HttpResponse({ status: 200, body: mockAddresses })).pipe(delay(this.DEFAULT_DELAY))
                 }
-                if (req.url.includes('orders/id')) {
-                    let header = new HttpHeaders();
-                    let var0=header.set('Location', '100')
-                    return of(new HttpResponse({ status: 200, headers: var0 })).pipe(delay(this.DEFAULT_DELAY))
-                }
-                if (req.url.includes('/confirm')) {
-                    return of(new HttpResponse({ status: 200, body: {paymentStatus:true} })).pipe(delay(this.DEFAULT_DELAY))
-                }
-                if (req.url.includes('orders/')) {
+                if (req.url.includes('orders/user/')) {
                     return of(new HttpResponse({ status: 200, body: mockOrder })).pipe(delay(this.DEFAULT_DELAY))
                 }
-                if (req.url.includes('orders')) {
+                if (req.url.includes('orders/user')) {
                     return of(new HttpResponse({ status: 200, body: mockOrders })).pipe(delay(this.DEFAULT_DELAY))
                 }
                 if (req.url.includes('filters')) {
